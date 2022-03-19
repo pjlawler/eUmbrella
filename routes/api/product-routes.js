69781d671cb8c0d82bd1919f293dedf738d0f69b
Pat Lawler <patlaw777@icloud.com>
2022-03-18 21:49:54 -0500

    added 'get' product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,11 +6,59 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [{
+      // be sure to include its associated Category and Tag data
+      // Category
+      model: Category,
+      attributes: ['id', 'category_name']
+    },       {
+      model: Tag,
+      attributes: ['tag_name'],
+      through: ProductTag,
+      as: 'product_tags'
+    }]
+  })
+  .then(dbProductData => res.json(dbProductData))
+  .catch(err => {
+    res.status(500).json(err)
+  })
 });
-
 // get one product
 router.get('/:id', (req, res) => {
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [{
+      // be sure to include its associated Category and Tag data
+      // Category
+      model: Category,
+      attributes: ['id', 'category_name']
+    },       {
+      model: Tag,
+      attributes: ['tag_name'],
+      through: ProductTag,
+      as: 'product_tags'
+    }]
+  })
+  .then(dbProductData => {
+    if(!dbProductData) {
+      res.status(404).json({message: 'No product found with this id'})
+      return;
+    } else {
+      res.json(dbProductData);
+    }
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+  
+
+
+
+
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
 });
