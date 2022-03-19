89d16263ec9decd86ae538cfcf3f678bdf5f6b69
Pat Lawler <patlaw777@icloud.com>
2022-03-18 21:02:33 -0500

    got setup and seeded

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const routes = require('./routes');
+
 // import sequelize connection
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,6 +11,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
+
+// sequelize.sync({ force: false }).then(() => {
+//   console.log('synched now starting server')
+//   app.listen(PORT, () => console.log('Now lstening'));
+// });
 
 // sync sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
